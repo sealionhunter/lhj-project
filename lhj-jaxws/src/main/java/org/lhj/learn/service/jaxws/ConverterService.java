b61f53944757d8a7944c2ea32ba7a6d54eaeca23
huiwenbin@yahoo.com.cn <huiwenbin@yahoo.com.cn@fd20581a-9d56-11de-b988-bd14ec207769>
2009-09-09 15:49:24 +0000

    init 20090909

---
+++ lhj-jaxws/src/main/java/org/lhj/learn/service/jaxws/ConverterService.java
@@ -0,0 +1,34 @@
+/*
+ * All Rights Reserved. Copyright(C) LHJ 2009. 
+ */
+package org.lhj.learn.service.jaxws;
+
+import java.math.BigDecimal;
+
+import javax.jws.WebService;
+
+/**
+ * Converter service
+ * @author sealion
+ *
+ */
+@WebService(endpointInterface = "org.lhj.learn.service.jaxws.Converter",
+        targetNamespace = "http://services.lhj.org/Converter",
+        serviceName = "ConverterService",
+        portName = "ConverterPortType")
+public class ConverterService implements Converter {
+    private BigDecimal rate = new BigDecimal(0.8);
+    private static final BigDecimal ZERO = new BigDecimal(0);
+
+    /**
+     * converte dollar to euro
+     * @param dollars dollars
+     * @return euro
+     */
+    @Override
+    public BigDecimal dollarToEuro(BigDecimal dollars) {
+        BigDecimal result = dollars == null ? ZERO : dollars.multiply(rate);
+        return result;
+    }
+
+}
