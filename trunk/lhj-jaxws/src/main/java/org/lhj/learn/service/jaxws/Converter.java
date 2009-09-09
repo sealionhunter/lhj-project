d51356a7819296292f322c4d5e3115957b0c9e00
huiwenbin@yahoo.com.cn <huiwenbin@yahoo.com.cn@fd20581a-9d56-11de-b988-bd14ec207769>
2009-09-09 15:49:24 +0000

    init 20090909

---
+++ trunk/lhj-jaxws/src/main/java/org/lhj/learn/service/jaxws/Converter.java
@@ -0,0 +1,25 @@
+/*
+ * All Rights Reserved. Copyright(C) LHJ 2009. 
+ */
+package org.lhj.learn.service.jaxws;
+
+import java.math.BigDecimal;
+
+import javax.jws.WebService;
+
+/**
+ * Converter service interface
+ * @author sealion
+ *
+ */
+@WebService(name="ConverterPortType",
+        targetNamespace="http://services.lhj.org/ConverterPortType")
+public interface Converter {
+    /**
+     * converte dollar to euro
+     * @param dollars dollars
+     * @return euro
+     */
+    BigDecimal dollarToEuro(BigDecimal dollars);
+
+}
