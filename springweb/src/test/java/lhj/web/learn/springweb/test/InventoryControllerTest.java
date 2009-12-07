d097c61505c776b1c84c688b86e08a1e0697d8e8
sealion.hunter@gmail.com <sealion.hunter@gmail.com@fd20581a-9d56-11de-b988-bd14ec207769>
2009-12-07 16:55:25 +0000

    update 20091208

--- springweb/src/test/java/lhj/web/learn/springweb/test/InventoryControllerTest.java
+++ springweb/src/test/java/lhj/web/learn/springweb/test/InventoryControllerTest.java
@@ -1,13 +1,9 @@
 package lhj.web.learn.springweb.test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
 import java.util.ArrayList;
 import java.util.Map;
 
 import jp.co.dgic.testing.framework.DJUnitTestCase;
-
 import lhj.web.learn.springweb.InventoryController;
 import lhj.web.learn.springweb.domain.Product;
 import lhj.web.learn.springweb.service.impl.SimpleProductManager;
@@ -18,20 +14,20 @@ import org.springframework.web.servlet.ModelAndView;
 
 public class InventoryControllerTest extends DJUnitTestCase {
 
-	@SuppressWarnings("unchecked")
-	@Test
-	public void testHandleRequest() throws Exception {
-		InventoryController controller = new InventoryController();
-		SimpleProductManager spm = new SimpleProductManager();
-		spm.setProductDAO(new InMemoryProductDAO(new ArrayList<Product>()));
-		controller.setProductManager(spm);
-		ModelAndView modelAndView = controller.handleRequest(null, null);
-		assertEquals("hello", modelAndView.getViewName());
-		assertNotNull(modelAndView.getModel());
-		
-		Map<String, Object> model = (Map) modelAndView.getModel().get("model");
-		String nowValue = (String) model.get("now");
-		assertNotNull(nowValue);
-	}
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testHandleRequest() throws Exception {
+        InventoryController controller = new InventoryController();
+        SimpleProductManager spm = new SimpleProductManager();
+        spm.setProductDAO(new InMemoryProductDAO(new ArrayList<Product>()));
+        controller.setProductManager(spm);
+        ModelAndView modelAndView = controller.handleRequest(null, null);
+        assertEquals("hello", modelAndView.getViewName());
+        assertNotNull(modelAndView.getModel());
+
+        Map<String, Object> model = (Map) modelAndView.getModel().get("model");
+        String nowValue = (String) model.get("now");
+        assertNotNull(nowValue);
+    }
 
 }
