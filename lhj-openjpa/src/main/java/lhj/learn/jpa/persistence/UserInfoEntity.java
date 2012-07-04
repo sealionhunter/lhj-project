d5a2fcb1a426e8fa76c45b6712db2e21a5ce9068
sealion.hunter@gmail.com <sealion.hunter@gmail.com@fd20581a-9d56-11de-b988-bd14ec207769>
2012-07-04 18:54:26 +0000

    fixed review issue

--- lhj-openjpa/src/main/java/lhj/learn/jpa/persistence/UserInfoEntity.java
+++ lhj-openjpa/src/main/java/lhj/learn/jpa/persistence/UserInfoEntity.java
@@ -22,6 +22,10 @@ import javax.persistence.Table;
     @NamedQuery(name = "UserInfoEntity.list", query = "select u from UserInfoEntity u")
 })
 public class UserInfoEntity implements Serializable {
+    /**
+     * serialVersionUID
+     */
+    private static final long serialVersionUID = 1489271554222108290L;
     @Id
     @Column(updatable = false, insertable = false, name = "ID")
     @GeneratedValue(strategy = IDENTITY)
@@ -34,7 +38,6 @@ public class UserInfoEntity implements Serializable {
     private String sex;
     @Column(name = "BIRTHDAY", nullable = false)
     private Date birthday;
-    private static final long serialVersionUID = 1L;
 
     public UserInfoEntity() {
         super();
@@ -44,7 +47,7 @@ public class UserInfoEntity implements Serializable {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(final long id) {
         this.id = id;
     }
 
@@ -52,7 +55,7 @@ public class UserInfoEntity implements Serializable {
         return userId;
     }
 
-    public void setUserId(String userId) {
+    public void setUserId(final String userId) {
         this.userId = userId;
     }
 
@@ -60,7 +63,7 @@ public class UserInfoEntity implements Serializable {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(final String name) {
         this.name = name;
     }
 
@@ -68,7 +71,7 @@ public class UserInfoEntity implements Serializable {
         return sex;
     }
 
-    public void setSex(String sex) {
+    public void setSex(final String sex) {
         this.sex = sex;
     }
 
@@ -76,13 +79,78 @@ public class UserInfoEntity implements Serializable {
         return birthday;
     }
 
-    public void setBirthday(Date birthday) {
+    public void setBirthday(final Date birthday) {
         this.birthday = birthday;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((birthday == null) ? 0 : birthday.hashCode());
+        result = prime * result + (int) (id ^ (id >>> 32));
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((sex == null) ? 0 : sex.hashCode());
+        result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+        return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof UserInfoEntity)) {
+            return false;
+        }
+        final UserInfoEntity other = (UserInfoEntity) obj;
+        if (birthday == null) {
+            if (other.birthday != null) {
+                return false;
+            }
+        } else if (!birthday.equals(other.birthday)) {
+            return false;
+        }
+        if (id != other.id) {
+            return false;
+        }
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        if (sex == null) {
+            if (other.sex != null) {
+                return false;
+            }
+        } else if (!sex.equals(other.sex)) {
+            return false;
+        }
+        if (userId == null) {
+            if (other.userId != null) {
+                return false;
+            }
+        } else if (!userId.equals(other.userId)) {
+            return false;
+        }
+        return true;
     }
 
     @Override
     public String toString() {
-        StringBuffer sb = new StringBuffer();
+        final StringBuffer sb = new StringBuffer();
         sb.append("userId:").append(this.getUserId());
         sb.append("|name:").append(this.getName());
         sb.append("|sex:").append(this.getSex());
