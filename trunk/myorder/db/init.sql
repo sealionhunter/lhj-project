3a82c2889562bf0f39450d0a1906406b22e703b5
sealion.hunter@gmail.com <sealion.hunter@gmail.com@fd20581a-9d56-11de-b988-bd14ec207769>
2012-09-21 03:18:26 +0000

    update db script

--- trunk/myorder/db/init.sql
+++ trunk/myorder/db/init.sql
@@ -1,2 +1,1 @@
 INSERT INTO `myhotel`.`userinfo` (`uid`, `address`, `birthday`, `email`, `isAdmin`, `mobile`, `password`, `sex`, `uname`, `validTo`) VALUES ('admin', '天河路', '1992/02/02', 'test@test.com', true, '0551-8888888', 'gnzLDuqKcGxMNKFokfhOew==', '1', '管理员', 'MjUzNDA0ODQ0MjU5MTkw');
-
