6f77f413488031b4a03b5cccd2bf6ac71c8b4c72
sealion.hunter@gmail.com <sealion.hunter@gmail.com@fd20581a-9d56-11de-b988-bd14ec207769>
2013-02-23 08:23:46 +0000

    add batch for dbupdate and add comfirm before do the batch

--- trunk/exam/db/dbrestore.bat
+++ trunk/exam/db/dbrestore.bat
@@ -2,6 +2,12 @@
 @setlocal
 pushd %~pd0
 
+set /p confirm=All the data will be lost. Are you sure?
+if /I '%confirm%' NEQ 'yes' exit /b 1;
+set /p confirm=Confire again?
+if /I '%confirm%' NEQ 'yes' exit /b 1;
+
+
 set /p password=please enter password:
 set /p backupfile=please enter backup file:
 
--- trunk/exam/db/setup.bat
+++ trunk/exam/db/setup.bat
@@ -2,6 +2,11 @@
 @setlocal
 pushd %~pd0
 
+set /p confirm=All the data will be lost. Are you sure?
+if /I '%confirm%' NEQ 'yes' exit /b 1;
+set /p confirm=Confire again?
+if /I '%confirm%' NEQ 'yes' exit /b 1;
+
 set /p password=please enter password:
 
 mysql -u root --password=%password% -e "source db.sql"
