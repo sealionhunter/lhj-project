a6b25216a87268319a97733d72b3b51036eda37f
shen427@gmail.com <shen427@gmail.com@fd20581a-9d56-11de-b988-bd14ec207769>
2013-03-04 16:37:03 +0000

    admission print modify jsp

--- exam/src/main/java/controller/AdmissionController.java
+++ exam/src/main/java/controller/AdmissionController.java
@@ -1,5 +1,10 @@
 package controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -8,15 +13,19 @@ import model.Apply;
 import model.User;
 
 import org.springframework.validation.BindException;
+import org.springframework.validation.Errors;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
 
 import service.RegistService;
 
 import command.AdmissionCommand;
+import command.RegistCommand;
 
 public class AdmissionController extends SimpleFormController {
     private RegistService registService;
+    private String beginDate;
+    private String endDate;
 
     /**
      * @return the registService
@@ -31,6 +40,17 @@ public class AdmissionController extends SimpleFormController {
      */
     public void setRegistService(RegistService registService) {
         this.registService = registService;
+    }
+
+    @Override
+    protected Map referenceData(HttpServletRequest request, Object command,
+            Errors errors) throws Exception {
+        Date now = new Date();
+        String pattern = "yyyy/MM/dd HH:mm:ss";
+        if (now.after(getEndTime(pattern)) || now.before(getBeginTime(pattern))) {
+            throw new Exception("当前时间段不在打印时间内！");
+        }
+        return super.referenceData(request, command, errors);
     }
 
     @Override
@@ -49,8 +69,8 @@ public class AdmissionController extends SimpleFormController {
             HttpSession session = request.getSession();
             session.setAttribute("UserInfo", cmd.getUser());
             session.setAttribute("ApplyInfo", cmd.getApply());
-            session.setAttribute("photoData" + cmd.getIdCardNo(),
-                    cmd.getUser().getPhoto());
+            session.setAttribute("photoData" + cmd.getIdCardNo(), cmd.getUser()
+                    .getPhoto());
             return new ModelAndView(getFormView(), errors.getModel());
         } else {
             HttpSession session = request.getSession();
@@ -61,5 +81,39 @@ public class AdmissionController extends SimpleFormController {
             registService.printAdmission(cmd, errors);
             return new ModelAndView(getSuccessView(), errors.getModel());
         }
+    }
+
+    public String getBeginDate() {
+        return beginDate;
+    }
+
+    public void setBeginDate(String beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    private Date getBeginTime(String pattern) throws Exception {
+        String timeStr = " 00:00:00";
+
+        return getTime(beginDate + timeStr, pattern);
+    }
+
+    private Date getEndTime(String pattern) throws Exception {
+        String timeStr = " 23:59:59";
+
+        return getTime(endDate + timeStr, pattern);
+    }
+
+    private Date getTime(String timeStr, String pattern) throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+
+        return sdf.parse(timeStr);
     }
 }
